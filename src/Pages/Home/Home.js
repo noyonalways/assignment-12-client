cf868c74dce199d34475f04a884a92a4999d5dfd
codernoyon <noyonrahman2003@gmail.com>
2022-05-22 20:18:44 +0600

    required modules install

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-2xl text-green-400 text-center">Hello world from Home</h2>
+        </div>
+    );
+};
+
+export default Home;
