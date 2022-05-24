0cadf0d4d3705d6066a78a8254c9d7985315e82b
codernoyon <noyonrahman2003@gmail.com>
2022-05-24 18:37:57 +0600

    blog page update

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import PageTitle from '../../Components/PageTitle/PageTitle';
+import Footer from '../../Shared/Footer/Footer';
+import SingleQuestion from './SingleQuestion/SingleQuestion';
+import Image from '../../Assets/Image/question.svg';
+import LoadingRipple from '../../Components/LoadingRipple/LoadingRipple';
 
 const Blogs = () => {
+    const [questions, setQuestions] = useState([]);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        (async () => {
+            setLoading(true)
+            const { data } = await axios.get('http://localhost:5000/question');
+            setQuestions(data.data);
+            setLoading(false);
+        })();
+    }, []);
+    
     return (
-        <div>
-            <PageTitle title={'Blogs'}/>
-            <div className="text-2xl text-center">This is Blogs page</div>
-        </div>
+        <section >
+            <PageTitle title={'Blogs'} />
+            {
+                loading ? <div className="h-screen flex items-center"><LoadingRipple /></div> : <div className="container mx-auto py-10 px-3 lg:px-0">
+                    <div className="flex justify-between flex-wrap">
+                        <div className='basis-full lg:basis-[40%]'>
+                            <img className='w-full' src={Image} alt="" />
+                        </div>
+                        <div className='space-y-3 basis-full lg:basis-[55%]'>
+                            {
+                                questions.map(item => <SingleQuestion item={item} />)
+                            }
+                        </div>
+                    </div>
+                </div>
+            }
+            <Footer />
+        </section>
     );
 };
 
---
+++ src/Pages/Blogs/SingleQuestion/SingleQuestion.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SingleQuestion = ({item}) => {
+    const {question, answer} = item;
+    return (
+        <div tabIndex="0" className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+            <div className="collapse-title text-xl font-medium">
+                {question}
+            </div>
+            <div className="collapse-content">
+                <p>{answer}</p>
+            </div>
+        </div>
+    );
+};
+
+export default SingleQuestion;
