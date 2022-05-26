61c015fb0b9fd19ef0a703c6cd58adda7bd97353
codernoyon <noyonrahman2003@gmail.com>
2022-05-27 01:12:33 +0600

    portfolio live api link added

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,35 +1,27 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PageTitle from '../../Components/PageTitle/PageTitle';
 import Footer from '../../Shared/Footer/Footer';
 import SingleQuestion from './SingleQuestion/SingleQuestion';
 import Image from '../../Assets/Image/question.svg';
 import LoadingRipple from '../../Components/LoadingRipple/LoadingRipple';
+import { useQuery } from 'react-query';
 
 const Blogs = () => {
-    const [questions, setQuestions] = useState([]);
-    const [loading, setLoading] = useState(false);
-    useEffect(() => {
-        (async () => {
-            setLoading(true)
-            const { data } = await axios.get('https://glacial-temple-86041.herokuapp.com/question');
-            setQuestions(data.data);
-            setLoading(false);
-        })();
-    }, []);
-    
+    const { data, isLoading } = useQuery('questions', async () => await axios.get('https://glacial-temple-86041.herokuapp.com/question'));
+    const questions = data?.data?.data;
     return (
         <section >
             <PageTitle title={'Blogs'} />
             {
-                loading ? <div className="h-screen flex items-center"><LoadingRipple /></div> : <div className="container mx-auto py-10 px-3 lg:px-0">
+                isLoading ? <div className="h-screen flex items-center"><LoadingRipple /></div> : <div className="container mx-auto py-10 px-3 lg:px-0">
                     <div className="flex justify-between flex-wrap">
                         <div className='basis-full lg:basis-[40%]'>
                             <img className='w-full' src={Image} alt="" />
                         </div>
                         <div className='space-y-3 basis-full lg:basis-[55%]'>
                             {
-                                questions.map(item => <SingleQuestion item={item} />)
+                                questions?.map(item => <SingleQuestion key={item._id} item={item} />)
                             }
                         </div>
                     </div>
--- src/Pages/Home/HomeBanner/HomeBanner.js
+++ src/Pages/Home/HomeBanner/HomeBanner.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import "swiper/css";
 import "swiper/css/pagination";
@@ -7,28 +7,20 @@ import SingleSlide from './SingleSlide/SingleSlide';
 import './HomeBanner.css'
 import axios from 'axios';
 import LoadingCircle from '../../../Components/LoadingCircle/LoadingCircle';
+import { useQuery } from 'react-query';
 
 
 const HomeBanner = () => {
-    const [banners, setBanners] = useState([]);
-    const [loading, setLoading] = useState(false);
-    useEffect(() => {
-        (async () => {
-            setLoading(true);
-            const { data } = await axios.get('https://glacial-temple-86041.herokuapp.com/home-slider');
-            setBanners(data.data);
-            setLoading(false);
-        })();
-    }, []);
-
+    const { data, isLoading } = useQuery('banners', async () => await axios.get('https://glacial-temple-86041.herokuapp.com/home-slider'));
+    const banners = data?.data?.data ? data.data.data : [];
 
 
     return (
         <div>
             {
-                loading ? <div className='py-5 h-screen flex items-center'><LoadingCircle /></div> : <Swiper pagination={{ dynamicBullets: true, grabCursor: true, }} modules={[Pagination]} className="mySwiper">
+                isLoading ? <div className='py-5 h-screen flex items-center'><LoadingCircle /></div> : <Swiper pagination={{ dynamicBullets: true, grabCursor: true, }} modules={[Pagination]} className="mySwiper">
                     {
-                        banners.map((item) => <SwiperSlide key={item._id}> <SingleSlide item={item} /></SwiperSlide>)
+                        banners?.map((item) => <SwiperSlide key={item._id}> <SingleSlide item={item} /></SwiperSlide>)
                     }
                 </Swiper>
             }
